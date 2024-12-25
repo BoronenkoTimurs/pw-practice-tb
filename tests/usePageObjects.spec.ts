9ec844c8fdd81aa085229c45f329a6621a719bba
BoronenkoTimurs <64987379+BoronenkoTimurs@users.noreply.github.com>
2024-12-25 13:02:36 +0200

    Change test title.

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async({page}) => {
     await page.goto('/')
 })
 
-test.only('navigate to form page @smoke @regression', async({page}) => {
+test.only('Test for testing CI', async({page}) => {
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage()
     await pm.navigateTo().datepickerPage()
